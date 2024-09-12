a1a46b48b5e63df87268b766f0d05a77c20648c4
Margarita Naidanova <rinema@mail.ru>
2024-09-12 20:07:15 +0700

    add linters

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/main.scss'
+import pic from './assets/2m8lm7v322e51.jpg'
+// import axios from "axios";
+
+// console.log(456455556)
+const laughImg = document.getElementById('laughImg')
+
+laughImg.src = pic
