bd7b136eeed09a68a223fd8bfcaaacfd7742fe73
Margarita Naidanova <rinema@mail.ru>
2024-09-12 21:09:09 +0700

    chore: linter formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
 export default [
-    {
-        files: ['src/**/*.js'],
-        ignores: ['**/*.config.js', '!**/eslint.config.js', '.node_modules/', '.dist/', '.build/'],
+  {
+    files: ['src/**/*.js'],
+    ignores: ['**/*.config.js', '!**/eslint.config.js', '.node_modules/', '.dist/', '.build/'],
 
-        rules: {
-            indent: ['warn', 2],
+    rules: {
+      indent: ['warn', 2],
 
-            'linebreak-style': [0, 'unix'],
-            quotes: ['warn', 'single'],
-            semi: ['warn', 'never'],
-            strict: 0,
-            endOfLine: 'off',
-        },
+      'linebreak-style': [0, 'unix'],
+      quotes: ['warn', 'single'],
+      semi: ['warn', 'never'],
+      strict: 0,
+      endOfLine: 'off',
     },
+  },
 ]
