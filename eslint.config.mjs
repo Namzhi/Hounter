6fa360000865188ddcb016778d806f1e361ff9e5
Margarita Naidanova <rinema@mail.ru>
2024-09-12 21:03:44 +0700

    feat: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+export default [
+    {
+        files: ['src/**/*.js'],
+        ignores: ['**/*.config.js', '!**/eslint.config.js', '.node_modules/', '.dist/', '.build/'],
+
+        rules: {
+            indent: ['warn', 2],
+
+            'linebreak-style': [0, 'unix'],
+            quotes: ['warn', 'single'],
+            semi: ['warn', 'never'],
+            strict: 0,
+            endOfLine: 'off',
+        },
+    },
+]
